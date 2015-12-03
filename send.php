c5fd59b59dfe30960bb408497255cf3884929133
tymurbaniak <tymentos@gmail.com>
2015-12-03 14:06:47 +0100

    Implemented functionality
    send.php - function finally sends data to database

--- send.php
+++ send.php
@@ -1,22 +1,12 @@
 <?php
 
 
-$globallink = connect_to_database();
-send_data_to_database($_POST);
-close_connection_with_database();
+connect_to_database();
 
 function connect_to_database(){
 	require('serverconf.php');
-	if(!($link = mysqli_connect($adresserwera, $nazwauzyt, $haslo)))
-	{return 'Server problem'; exit(0);}
+	$link = mysqli_connect($adresserwera, $nazwauzyt, $haslo, $nazwabd);
 
-	if(!mysqli_select_db($link, $nazwabd))
-	{return 'Database problem';}
-
-	return $link;
-}
-function send_data_to_database($array){
-	global $globallink;
 	$date = date('d-m-Y H:i:s');
 	$inc =0.5;
 	$wrfname = $_POST['writersName'];
@@ -26,21 +16,21 @@ function send_data_to_database($array){
 			$ncounter = "name".$incr;
 			$quantity = $_POST[$qcounter];
 			$name = $_POST[$ncounter];
-			$zapytanie = "INSERT INTO sf (id, date, name, quantity, wrfname, wrflname) VALUES (NULL, , '".$date."', '".$name."', '".$quantity."','".$wrfname."','".$wrflname."') ";
-			$wynik_zapytania = mysqli_query($globallink, $zapytanie);
+			$zapytanie = "INSERT INTO sf (id, date, name, quantity, wrfname, wrlname) VALUES (NULL, CURRENT_TIMESTAMP, '".$name."', '".$quantity."','".$wrfname."','".$wrflname."') ";
+			echo $zapytanie. "<br>";
+			echo $date." ".$name." ".$quantity." ".$wrfname."<br>";
 			
-			if(!$wynik_zapytania) { 
+			$wynik_zapytania = mysqli_query($link, $zapytanie);
+			echo mysqli_connect_error();
+			
+			if(mysqli_connect_errno()) { 
 				echo "Nie dodano<br>";
-				echo $wynik_zapytania;
-				echo $wynik_zapytania;
-				return 'Problem z dopisaniem obrazka do bazy';
+				return 'Nie dzia³a :o';
 			}else{
 				echo "Dodano rekord do bazy";
 			} 
 		}
-}
-function close_connection_with_database(){
-	global $globallink;
-	if(!mysqli_close($globallink)) {return 'Problem z zamknieciem bazy';}
+
+	if(!mysqli_close($link)) {return 'Problem z zamknieciem bazy';}
 }
 ?>
