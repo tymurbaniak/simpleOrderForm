41536fda23df1428fbf5410d7e08166ef6b7bd4c
tymurbaniak <tymentos@gmail.com>
2015-12-04 10:48:05 +0100

    Changing structure of database
    Structure of datanase has changed, so there was necessary to change some
    code.

--- send.php
+++ send.php
@@ -1,26 +1,42 @@
 <?php
 
+send_data_to_database();
 
-connect_to_database();
-
-function connect_to_database(){
+function send_data_to_database(){
 	require('serverconf.php');
 	$link = mysqli_connect($adresserwera, $nazwauzyt, $haslo, $nazwabd);
 
-	$date = date('d-m-Y H:i:s');
-	$inc =0.5;
 	$wrfname = $_POST['writersName'];
 	$wrflname = $_POST['writersLName'];
+	
+	$zapytanie_zamowienie = "
+		INSERT INTO `sf`.`order` (`order_id`, `order_fname`, `order_lname`, `order_date`) VALUES (NULL, '".$wrfname."', '".$wrflname."', CURRENT_TIMESTAMP);
+		";
+	$wynik_zapytania = mysqli_query($link, $zapytanie_zamowienie);
+	echo mysqli_connect_error();
+			
+	if(mysqli_connect_errno()) { 
+		echo "Nie dodano<br>";
+		return 'Nie dzia³a :o';
+	}else{
+		echo "Dodano rekord do bazy";
+	} 
+		
+	$foreignkey = $link->insert_id;
+	echo $foreignkey;
 		for($incr=1; $incr<12; $incr++){
 			$qcounter = "quantity".$incr;
 			$ncounter = "name".$incr;
 			$quantity = $_POST[$qcounter];
 			$name = $_POST[$ncounter];
-			$zapytanie = "INSERT INTO sf (id, date, name, quantity, wrfname, wrlname) VALUES (NULL, CURRENT_TIMESTAMP, '".$name."', '".$quantity."','".$wrfname."','".$wrflname."') ";
-			echo $zapytanie. "<br>";
-			echo $date." ".$name." ".$quantity." ".$wrfname."<br>";
+			$zapytanie_produkt = "
+			INSERT INTO product (product_id, product_pname, product_quantity, product_price, product_forordkey) 
+			VALUES (NULL, '".$name."','".$quantity."', '', '".$foreignkey."');
+			";
+			echo $zapytanie_produkt. "<br>";
+			echo $name." ".$quantity." ".$foreignkey."<br>";
 			
-			$wynik_zapytania = mysqli_query($link, $zapytanie);
+			$wynik_zapytania = mysqli_query($link, $zapytanie_produkt);
 			echo mysqli_connect_error();
 			
 			if(mysqli_connect_errno()) { 
@@ -30,6 +46,7 @@ function connect_to_database(){
 				echo "Dodano rekord do bazy";
 			} 
 		}
+		
 
 	if(!mysqli_close($link)) {return 'Problem z zamknieciem bazy';}
 }
